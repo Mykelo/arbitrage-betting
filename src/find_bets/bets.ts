35c4c8e71e2825c6037426b0685b228cc4ad810a
Mykelo <michal.mieszko.wawrzyniak@gmail.com>
2021-09-27 01:47:01 +0200

    finding bets improved

--- src/find_bets/bets.ts
+++ src/find_bets/bets.ts
@@ -2,50 +2,41 @@ import { Bet, BetGroup, BookmakerEvent, SportEvent } from '../types';
 
 export function findProfitableBets(event: SportEvent): BetGroup[] {
   const profitableBets: BetGroup[] = [];
-  for (let i = 0; i < event.bookmakerEvents.length; i++) {
-    const bookmakerEvent1 = event.bookmakerEvents[i];
-    for (let j = 0; j < event.bookmakerEvents.length; j++) {
-      if (i !== j) {
-        const bookmakerEvent2 = event.bookmakerEvents[j];
-        for (let k = 0; k < bookmakerEvent1.odds.length; k++) {
-          const newOdds = bookmakerEvent2.odds.slice();
-          newOdds.splice(k, 1, bookmakerEvent1.odds[k]);
 
-          const percentage = newOdds.reduce((prev, curr) => prev + 1 / curr, 0);
-          if (percentage < 1.02 || true) {
-            const bets: Bet[] = newOdds.map((odd, index) => {
-              const bookmakerEvent =
-                index === k ? bookmakerEvent1 : bookmakerEvent2;
-              const bookmaker = bookmakerEvent.bookamerName || 'n/a';
-              if (index === 0) {
-                return {
-                  team: event.teams[0],
-                  odd,
-                  bookmaker,
-                };
-              } else if (index === 1) {
-                return {
-                  team: 'draw',
-                  odd,
-                  bookmaker,
-                };
-              } else {
-                return {
-                  team: event.teams[1],
-                  odd,
-                  bookmaker,
-                };
-              }
-            });
+  const max = (bets: Bet[]) =>
+    bets.reduce(function (prev, current) {
+      return prev.odd > current.odd ? prev : current;
+    });
 
-            profitableBets.push({
-              bets: bets.slice(0, 3) as [Bet, Bet, Bet],
-              percentage: percentage * 100,
-            });
-          }
-        }
-      }
-    }
+  const team1Odds: Bet[] = event.bookmakerEvents.map(e => ({
+    odd: e.odds[0],
+    team: event.teams[0],
+    bookmaker: e.bookamerName || 'n/a',
+  }));
+
+  const drawOdds: Bet[] = event.bookmakerEvents.map(e => ({
+    odd: e.odds[1],
+    team: 'draw',
+    bookmaker: e.bookamerName || 'n/a',
+  }));
+
+  const team2Odds: Bet[] = event.bookmakerEvents.map(e => ({
+    odd: e.odds[2],
+    team: event.teams[1],
+    bookmaker: e.bookamerName || 'n/a',
+  }));
+
+  const team1Best = max(team1Odds);
+  const drawBest = max(drawOdds);
+  const team2Best = max(team2Odds);
+
+  const percentage = 1 / team1Best.odd + 1 / drawBest.odd + 1 / team2Best.odd;
+
+  if (percentage < 1) {
+    profitableBets.push({
+      bets: [team1Best, drawBest, team2Best] as [Bet, Bet, Bet],
+      percentage: percentage * 100,
+    });
   }
 
   return profitableBets;
